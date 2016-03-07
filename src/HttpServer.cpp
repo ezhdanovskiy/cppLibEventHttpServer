5d45c4e1d05d02280472c781cda0b1d261f6ccfc
Evgeny Zhdanovskiy <7323@list.ru>
2016-03-07 20:21:03 +0300

    more logs

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -7,33 +7,39 @@
 
 int runHttpServer()
 {
-  if (!event_init())
-  {
+  std::cout << __func__ << " start" << std::endl;
+
+  if (!event_init()) {
     std::cerr << "Failed to init libevent." << std::endl;
     return -1;
   }
+
   char const SrvAddress[] = "127.0.0.1";
   std::uint16_t SrvPort = 5555;
   std::unique_ptr<evhttp, decltype(&evhttp_free)> Server(evhttp_start(SrvAddress, SrvPort), &evhttp_free);
-  if (!Server)
-  {
+  if (!Server) {
     std::cerr << "Failed to init http server." << std::endl;
     return -1;
   }
-  void (*OnReq)(evhttp_request *req, void *) = [] (evhttp_request *req, void *)
-  {
+  std::cout << "Server inited" << std::endl;
+
+  void (*OnReq)(evhttp_request *req, void *) = [] (evhttp_request *req, void *) {
     auto *OutBuf = evhttp_request_get_output_buffer(req);
-    if (!OutBuf)
+    if (!OutBuf) {
+      std::cerr << "evhttp_request_get_output_buffer return NULL" << std::endl;
       return;
+    }
     evbuffer_add_printf(OutBuf, "<html><body><center><h1>Hello Wotld!01</h1></center></body></html>");
     evhttp_send_reply(req, HTTP_OK, "", OutBuf);
   };
   evhttp_set_gencb(Server.get(), OnReq, nullptr);
+
   std::cout << "before event_dispatch" << std::endl;
-  if (event_dispatch() == -1)
-  {
+  if (event_dispatch() == -1) {
     std::cerr << "Failed to run messahe loop." << std::endl;
     return -1;
   }
+
+  std::cout << __func__ << " return 0" << std::endl;
   return 0;
 }
