810967817d23077c808cbc361d7f529d16e65486
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-03-29 21:16:12 +0300

    refactoring

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -1,14 +1,12 @@
 #include "HttpServer.h"
 
 #include <memory>
-#include <cstdint>
 #include <iostream>
 #include <evhttp.h>
 #include <iomanip>
-#include <ctime>
 #include <sstream>
 
-std::string strtime02()
+std::string getTime()
 {
     time_t rawtime;
     struct tm * timeinfo;
@@ -18,9 +16,21 @@ std::string strtime02()
     timeinfo = localtime(&rawtime);
 
     strftime(buffer, 80 ,"%d-%m-%Y %I:%M:%S", timeinfo);
-
-    return buffer;
+    std::cout << buffer << std::endl;
+    return std::string(buffer);
 }
+
+void onReq(evhttp_request *req, void *) {
+    auto *OutBuf = evhttp_request_get_output_buffer(req);
+    if (!OutBuf) {
+        std::cerr << "evhttp_request_get_output_buffer return NULL" << std::endl;
+        return;
+    }
+    std::stringstream answer;
+    answer << "<html><body><center><h1>" << "Hello Wotld! " << getTime() << "</h1></center></body></html>";
+    evbuffer_add_printf(OutBuf, answer.str().c_str());
+    evhttp_send_reply(req, HTTP_OK, "", OutBuf);
+};
 
 int runHttpServer() {
     std::cout << __func__ << " start" << std::endl;
@@ -39,18 +49,7 @@ int runHttpServer() {
     }
     std::cout << "Server inited" << std::endl;
 
-    void (*OnReq)(evhttp_request *req, void *) = [](evhttp_request *req, void *) {
-        auto *OutBuf = evhttp_request_get_output_buffer(req);
-        if (!OutBuf) {
-            std::cerr << "evhttp_request_get_output_buffer return NULL" << std::endl;
-            return;
-        }
-        std::stringstream answer;
-        answer << "<html><body><center><h1>" << "Hello Wotld! " << strtime02() << "</h1></center></body></html>";
-        evbuffer_add_printf(OutBuf, answer.str().c_str());
-        evhttp_send_reply(req, HTTP_OK, "", OutBuf);
-    };
-    evhttp_set_gencb(Server.get(), OnReq, nullptr);
+    evhttp_set_gencb(Server.get(), onReq, nullptr);
 
     std::cout << "before event_dispatch" << std::endl;
     if (event_dispatch() == -1) {
